b0f0c8d560b544731af1cfaa1eb1c318d3eab13a
Thiago Lagden <lagden@gmail.com>
2011-12-15 14:08:01 -0200

    comentando o script de grid

---
+++ apps/frontend/modules/sitemap/actions/actions.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * sitemap actions.
+ *
+ * @package    sfProject
+ * @subpackage sitemap
+ * @author     Thiago Lagden
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class sitemapActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->forward('default', 'module');
+  }
+}
