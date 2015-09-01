c25fce870ae1022d4c6589d7e2f395dc4f4f7d7f
Thiago Lagden <lagden@gmail.com>
2015-09-01 18:33:33 -0300

    ajustes nas imagens sem referencia de versão

--- lib/model/doctrine/Image.class.php
+++ lib/model/doctrine/Image.class.php
@@ -16,148 +16,152 @@ use lagden\GetFile as GetFile;
 
 class Image extends BaseImage
 {
-  static private function loadFormats()
-  {
-    $fmts = Doctrine_Core::getTable('Format')->findAll();
-    if ($fmts->count()==0) {
-      $fmts = new Doctrine_Collection('Format');
-      $formats = ['S','S2x','T','T2x','M','M2x','B','B2x'];
-      foreach ($formats as $format) {
-        $f = new Format();
-        $f->name=$format;
-        $f->save();
-        $fmts->add($f);
-      }
-    }
-    return $fmts;
-  }
+	static private function loadFormats()
+	{
+		$fmts = Doctrine_Core::getTable('Format')->findAll();
+		if ($fmts->count()==0) {
+			$fmts = new Doctrine_Collection('Format');
+			$formats = ['S','S2x','T','T2x','M','M2x','B','B2x'];
+			foreach ($formats as $format) {
+				$f = new Format();
+				$f->name=$format;
+				$f->save();
+				$fmts->add($f);
+			}
+		}
+		return $fmts;
+	}
 
-  protected function generateVersions($dir, $hash)
-  {
-    $ds       = DIRECTORY_SEPARATOR;
-    $versions = new Doctrine_Collection('ImageVersion');
-    $formats  = static::loadFormats();
-    $base     = "{$dir}{$ds}{$hash}{$ds}{$this->id}";
+	protected function generateVersions($dir, $hash)
+	{
+		$ds       = DIRECTORY_SEPARATOR;
+		$versions = new Doctrine_Collection('ImageVersion');
+		$formats  = static::loadFormats();
+		$base     = "{$dir}{$ds}{$hash}{$ds}{$this->id}";
 
-    foreach ($formats as $format) {
-      foreach (glob("{$base}{$ds}{$this->estate_id}-{$format->slug}.*") as $file)
-      {
-        if (is_file($file))
-        {
-          $file = basename($file);
-          $filename = "{$hash}/{$this->id}/$file";
+		foreach ($formats as $format) {
+			foreach (glob("{$base}{$ds}{$this->estate_id}-{$format->slug}.*") as $file)
+			{
+				if (is_file($file))
+				{
+					$file = basename($file);
+					$filename = "{$hash}/{$this->id}/$file";
 
-          // Generate the version
-          $version = new ImageVersion();
-          $version
-            ->setImage($this)
-            ->setFormat($format)
-            ->setFile($filename);
+					// Generate the version
+					$version = new ImageVersion();
+					$version
+						->setImage($this)
+						->setFormat($format)
+						->setFile($filename);
 
-          $versions->add($version);
-        }
-      }
-    }
-    return $versions;
-  }
+					$versions->add($version);
+				}
+			}
+		}
+		return $versions;
+	}
 
-  public function save(Doctrine_Connection $conn = null)
-  {
-    // No id, no FUN
-    if ($this->isNew()) {
-      parent::save($conn);
-    }
+	public function save(Doctrine_Connection $conn = null)
+	{
+		// No id, no FUN
+		if ($this->isNew()) {
+			parent::save($conn);
+		}
 
-    if ($this->file && (strpos($this->file, '/') === false)) {
+		if ($this->file && (strpos($this->file, '/') === false)) {
 
-      // Salva Imagem
-      $dir = $this->getTable()->dir();
-      $param = [
-        'id' => $this->id,
-        'name' => $this->file
-      ];
+			// Salva Imagem
+			$dir = $this->getTable()->dir();
+			$param = [
+				'id' => $this->id,
+				'name' => $this->file
+			];
 
-      $is = SaveFile::save(
-        $param,
-        $dir,
-        '{*}',
-        true,
-        'blowimg',
-        $this->estate_id
-      );
+			$is = SaveFile::save(
+				$param,
+				$dir,
+				'{*}',
+				true,
+				'blowimg',
+				$this->estate_id
+			);
 
-      $this->file = "{$is['hash']}/{$this->id}/{$this->estate_id}.{$is['ext']}";
+			$this->file = "{$is['hash']}/{$this->id}/{$this->estate_id}.{$is['ext']}";
 
-      $this->Versions->delete();
-      $this->Versions = $this->generateVersions($dir, $is['hash']);
-    }
+			$this->Versions->delete();
+			$this->Versions = $this->generateVersions($dir, $is['hash']);
+		}
 
-    return parent::save($conn);
-  }
+		return parent::save($conn);
+	}
 
-  private function version($type)
-  {
-    foreach ($this->Versions as $version) {
-      if($type === $version->Format->slug) {
-        return $version->file;
-      }
-    }
-    return null;
-  }
+	private function version($type)
+	{
+		foreach ($this->Versions as $version) {
+			if($type === $version->Format->slug) {
+				return $version->file;
+			}
+		}
+		return null;
+	}
 
-  public function formato($type)
-  {
-    $formats = [
-      'b2x' => 'large',
-      'b'   => 'large',
-      'm2x' => 'medium1',
-      'm'   => 'medium2',
-      't2x' => 'medium1',
-      't'   => 'medium2',
-      's2x' => 'small',
-      's'   => 'thumbnail',
-    ];
-    $ver = static::version($type);
-    $verOld = static::version($formats[$type]);
-    if ($ver) {
-      return "/uploads/{$this->getTable()->className()}/{$ver}";
-    }
-    if ($verOld) {
-      return "/estates/{$verOld}";
-    }
-    return null;
-  }
+	public function formato($type)
+	{
+		$ds = DIRECTORY_SEPARATOR;
+		$formats = [
+			'b2x' => 'large',
+			'b'   => 'large',
+			'm2x' => 'medium1',
+			'm'   => 'medium2',
+			't2x' => 'medium1',
+			't'   => 'medium2',
+			's2x' => 'small',
+			's'   => 'thumbnail',
+		];
+		$ver = static::version($type);
+		$verOld = static::version($formats[$type]);
+		if ($ver) {
+			return "/uploads/{$this->getTable()->className()}/{$ver}";
+		}
+		if ($verOld) {
+			return "/estates/{$verOld}";
+		}
+		if (file_exists(static::dir() . "{$this->id}{$ds}$formats[$type].jpg")) {
+			return "/estates/{$this->id}{$ds}$formats[$type].jpg";
+		}
+		return null;
+	}
 
-  public function postDelete($event)
-  {
-    $o = $event->getInvoker();
-    if($o->id) {
-      $dir = SaveFile::encontreNoGrupo($o->getTable()->dir(), $o->id);
-      if(is_dir($dir)) {
-        sfToolkit::clearDirectory($dir);
-        rmdir($dir);
-      }
-    }
-  }
+	public function postDelete($event)
+	{
+		$o = $event->getInvoker();
+		if($o->id) {
+			$dir = SaveFile::encontreNoGrupo($o->getTable()->dir(), $o->id);
+			if(is_dir($dir)) {
+				sfToolkit::clearDirectory($dir);
+				rmdir($dir);
+			}
+		}
+	}
 
-  // Old stuff compatibility
-  static public function dir()
-  {
-    $ds = DIRECTORY_SEPARATOR;
-    $web = sfConfig::get('sf_web_dir');
-    return "{$web}{$ds}estates{$ds}";
-  }
+	// Old stuff compatibility
+	static public function dir()
+	{
+		$ds = DIRECTORY_SEPARATOR;
+		$web = sfConfig::get('sf_web_dir');
+		return "{$web}{$ds}estates{$ds}";
+	}
 
-  public function delete(Doctrine_Connection $conn = null)
-  {
-    if ($this->id) {
-      // Removing images when deleting the record
-      $dir = static::dir().$this->id;
-      if(is_dir($dir)) {
-        sfToolkit::clearDirectory($dir);
-        rmdir($dir);
-      }
-    }
-    return parent::delete();
-  }
+	public function delete(Doctrine_Connection $conn = null)
+	{
+		if ($this->id) {
+			// Removing images when deleting the record
+			$dir = static::dir().$this->id;
+			if(is_dir($dir)) {
+				sfToolkit::clearDirectory($dir);
+				rmdir($dir);
+			}
+		}
+		return parent::delete();
+	}
 }
